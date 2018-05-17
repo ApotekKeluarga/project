0dc031d8fb8cfb0b0993e0be77a255c4be50aa7f
Microsoft-PC\Microsoft <firdaaisyah22@gmail.com>
2018-05-17 08:23:10 +0700

    ni

--- WEBCI/application/views/Home/Sidebar.php
+++ WEBCI/application/views/Home/Sidebar.php
@@ -6,7 +6,7 @@
           <ul class="site-menu" data-plugin="menu">
             
             <li class="site-menu-item active">
-              <a href="#">
+              <a href="<?php echo base_url() ?>index.php/Web/Dashboard">
                                 <i class="site-menu-icon md-view-dashboard" aria-hidden="true"></i>
                                 <span class="site-menu-title">Dashboard</span>
               </a>
